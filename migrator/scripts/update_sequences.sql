818444e88fa703d8b01f9188834d7dc9caee31b4
Alex Ioannidis <a.ioannidis@cern.ch>
2023-10-12 02:35:58 +0200

    migrator: bump recid sequence to 10M

--- migrator/scripts/update_sequences.sql
+++ migrator/scripts/update_sequences.sql
@@ -5,7 +5,7 @@ SELECT setval(pg_get_serial_sequence('files_location', 'id'), COALESCE(max(id) +
 SELECT setval(pg_get_serial_sequence('oaiserver_set', 'id'), COALESCE(max(id) + 1, 1), false) FROM oaiserver_set;
 SELECT setval(pg_get_serial_sequence('pages_page', 'id'), COALESCE(max(id) + 1, 1), false) FROM pages_page;
 SELECT setval(pg_get_serial_sequence('pidstore_pid', 'id'), COALESCE(max(id) + 1, 1), false) FROM pidstore_pid;
-SELECT setval(pg_get_serial_sequence('pidstore_recid', 'recid'), COALESCE(max(recid) + 1, 1), false) FROM pidstore_recid;
+SELECT setval(pg_get_serial_sequence('pidstore_recid', 'recid'), 10000000, false) FROM pidstore_recid;
 SELECT setval(pg_get_serial_sequence('request_number_seq', 'value'), COALESCE(max(value) + 1, 1), false) FROM request_number_seq;
 SELECT setval(pg_get_serial_sequence('access_actionsroles', 'id'), COALESCE(max(id) + 1, 1), false) FROM access_actionsroles;
 SELECT setval(pg_get_serial_sequence('access_actionsusers', 'id'), COALESCE(max(id) + 1, 1), false) FROM access_actionsusers;
